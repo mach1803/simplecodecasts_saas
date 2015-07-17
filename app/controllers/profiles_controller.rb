d1851c1ec9bdd50e9d37b94451ad785078425f04
Jonathan Reedy <jmr1706@gmail.com>
2015-07-17 19:10:26 +0000

    Changed profile creation method in ProfilesController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
        # form where a user can fill out there own profile
        @user = User.find(params[:user_id])
        
-       @profile = @user.build_profile
+       @profile = Profile.new
        
     end
     
